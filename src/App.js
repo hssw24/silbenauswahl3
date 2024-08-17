b69316619a5f3efc09231a118733c56f4f26f694
hssw24 <hssw@gmx.de>
2024-08-17 16:30:13 +0200

    old

--- src/App.js
+++ src/App.js
@@ -41,9 +41,6 @@ function App() {
         <SyllableButton index={0} onSpeak={speakSyllable} />
         <SyllableButton index={1} onSpeak={speakSyllable} />
         <SyllableButton index={2} onSpeak={speakSyllable} />
-        <SyllableButton index={0} onSpeak={speakSyllable} />
-        <SyllableButton index={1} onSpeak={speakSyllable} />
-        <SyllableButton index={2} onSpeak={speakSyllable} />
       </div>
       <p>Sprachausgabe ist wirklich schrottig!</p>
     </div>
