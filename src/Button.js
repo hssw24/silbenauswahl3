d69ff459ffd0f9c24cf19419a6610ef9a909f48a
hssw24 <hssw@gmx.de>
2024-08-17 12:54:50 +0200

    3x30

--- src/Button.js
+++ src/Button.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 
 const syllables = [
-  ["BA", "Ba", "FU", "KA", "ME", "Me", "NI", "Ni", "ZU", "XI"],
-  ["BO", "DA", "FI", "KU", "MA", "NO", "SU", "TA", "ZA", "XY"],
-  ["BIN", "DER", "FON", "KIM", "MON", "NES", "SIM", "TOR", "ZEN", "NUR"]
-];
+  ["BA", "BE", "BI", "BO", "BU", "DA", "DE", "DI", "DO", "DU", "FA", "FE", "FI", "FO", "FU", "GA", "GE", "GI", "GO", "GU", "HA", "HE", "HI", "HO", "HU", "JA", "JE", "JI", "JO", "JU"],
+  ["KA", "KE", "KI", "KO", "KU", "LA", "LE", "LI", "LO", "LU", "MA", "ME", "MI", "MO", "MU", "NA", "NE", "NI", "NO", "NU", "PA", "PE", "PI", "PO", "PU", "RA", "RE", "RI", "RO", "RU"],
+  ["SA", "SE", "SI", "SO", "SU", "TA", "TE", "TI", "TO", "TU", "VA", "VE", "VI", "VO", "VU", "WA", "WE", "WI", "WO", "WU", "ZA", "ZE", "ZI", "ZO", "ZU", "PI", "MA", "PA", "PO", "LA"]
+  ];
 
 function SyllableButton({ index, onSpeak }) {
   const [currentSyllable, setCurrentSyllable] = useState(syllables[index][0]);
